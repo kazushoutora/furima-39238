752aeabe7b33ce523568cf03a2cc14cc975ecbb1
kazushoutora <tomoyo.sato0830@gmail.com>
2023-05-24 18:37:14 +0900

    新規登録、ログイン、ログアウト設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  validates :text, presence: true
+  validates :nickname,           presence: true
+  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :first_name_yomi,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_yomi,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date,         presence: true
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
