359b0a7e686e5e2f1010eee4379b5a7b45764fff
kazushoutora <tomoyo.sato0830@gmail.com>
2023-05-26 17:56:29 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英数を両方含む必要があります'
-  validates :first_name,         presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/, message: 'は漢字・ひらがな・カタカナで入力してください' }
-  validates :last_name,          presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/, message: 'は漢字・ひらがな・カタカナで入力してください' }
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は漢字・ひらがな・カタカナで入力してください' }
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は漢字・ひらがな・カタカナで入力してください' }
   validates :first_name_yomi,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/,           message: '全角カタカナのみで入力して下さい' }
   validates :last_name_yomi,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/,           message: '全角カタカナのみで入力して下さい' }
   validates :birth_date,         presence: true
