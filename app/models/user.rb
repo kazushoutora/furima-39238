e1e4136a8902c109ed7861dbc84de4e9605f5aa4
kazushoutora <tomoyo.sato0830@gmail.com>
2023-05-26 15:39:11 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   validates :nickname,           presence: true
-  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
-  validates :first_name_yomi,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_yomi,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英数を両方含む必要があります' 
+  validates :first_name,         presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/, message: "は漢字・ひらがな・カタカナで入力してください" }
+  validates :last_name,          presence: true, format: { with: /\A[一-龯ぁ-んァ-ヶー－]+\z/, message: "は漢字・ひらがな・カタカナで入力してください" }
+  validates :first_name_yomi,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/,           message: "全角カタカナのみで入力して下さい"} 
+  validates :last_name_yomi,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/,           message: "全角カタカナのみで入力して下さい"}
   validates :birth_date,         presence: true
   
   devise :database_authenticatable, :registerable,
