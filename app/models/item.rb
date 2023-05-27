b091dd95af7ec4b8a6d404f19008f54f678c8be5
kazushoutora <tomoyo.sato0830@gmail.com>
2023-05-27 15:59:57 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,17 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :image,               presence: true
+  validates :item_name,           presence: true,
+  validates :item_description,    presence: true,
+  validates :category_id,         presence: true, 
+  validates :condition_id,        presence: true, 
+  validates :delivery_charges_id, presence: true, 
+  validates :delivery_from_id,    presence: true, 
+  validates :delivery_time_id,    presence: true, 
+  validates :price,               presence: true,
+
+  
+  has_one_attached :image
   belongs_to :user
   belongs_to :category_id
   belongs_to :condition_id
