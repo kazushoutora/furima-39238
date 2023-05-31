c06dcf75fa1ccbddec39646f371da461c9d81bbc
kazushoutora <tomoyo.sato0830@gmail.com>
2023-05-31 10:31:12 +0900

    販売手数料や販売利益

---
+++ app/javascript/item_kakaku.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue)
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    const addProfitDom = document.getElementById("profit");
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1)
+    addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1))
+  });
+})
+
