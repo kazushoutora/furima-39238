234e5a4211bd2b38ef7de8dd9b6596b58aebb07c
kazushoutora <tomoyo.sato0830@gmail.com>
2023-06-01 19:51:35 +0900

    画像詳細ページへ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   get 'items/index'
-  resources :items, only: [:index, :new, :create] do
+  resources :items, only: [:index, :new, :create, :show] do
   end  
 end
