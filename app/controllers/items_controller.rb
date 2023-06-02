029a2e042f120a6271420ea22b7c0ca9dfe6f439
kazushoutora <tomoyo.sato0830@gmail.com>
2023-06-02 19:18:44 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
 
@@ -33,6 +33,13 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    return unless @item.user_id == current_user.id
+
+    @item.destroy
+    redirect_to root_path
   end
 
   private
