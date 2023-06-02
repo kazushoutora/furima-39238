9bc069c9184d95e8f0f3a5b8bb4a900ea3e6149d
kazushoutora <tomoyo.sato0830@gmail.com>
2023-06-02 20:27:01 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,8 +38,11 @@ class ItemsController < ApplicationController
   def destroy
     return unless @item.user_id == current_user.id
 
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
