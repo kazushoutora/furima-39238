9dfe88926b3f576920457851c539106ea09d6e7c
kazushoutora <tomoyo.sato0830@gmail.com>
2023-06-02 14:18:12 +0900

    商品詳細表示機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -22,14 +22,15 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  
+
   private
+
   def item_params
-    params.require(:item).permit(:item_name, :item_description, :category_id, :condition_id, :delivery_charge_id, :delivery_from_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :item_description, :category_id, :condition_id, :delivery_charge_id,
+                                 :delivery_from_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def set_item
     @item = Item.find(params[:id])
   end
-
 end
